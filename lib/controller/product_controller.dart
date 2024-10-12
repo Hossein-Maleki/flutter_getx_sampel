8f2c4d8d33fdc8c8357767cb6e752e7595843d5b
codify <hosseinmaleki.dev@gmail.com>
2024-10-12 22:53:49 +0330

    lastcommit

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_getx_sampel/models/product_model.dart';
+import 'package:get/get.dart';
+
+class ProductController {
+  Rx<ProductMpdel>? productModel;
+  ProductController({ required this.productModel});
+}
--- lib/view/product_screen copy.dart
+++ lib/view/product_screen copy.dart
@@ -1,29 +1,22 @@
-//descreption:
-//we have 3 consebt in the project:
-
-//1.Dependency Injection -> in the line 16-18;
-
-//2.State manegment -> ractive(RX)+obserable(abs) variable ->obx object for a ractive widget;
-
-//3.MVC ararchitectural-> contoroler for conction modele <Layer>( data and Business Logiclogic) and view<Layer> )
-
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_sampel/controller/product_controller.dart';
 import 'package:flutter_getx_sampel/models/product_model.dart';
-import 'package:flutter_getx_sampel/view/product_screen%20copy.dart';
+import 'package:flutter_getx_sampel/view/main_screen.dart';
 import 'package:get/get.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     //طزریق مستقیم وابستگی
     // Rx<ProductMpdel> product = ProductMpdel(name: "apple", off: "50%", prise: "20000").obs;
+
+    //dependency injection
     ProductController _productcontroller = Get.put(ProductController(
         productModel:
             ProductMpdel(name: "laptop", prise: "200000", off: "20").obs));
-
+                  
     return Scaffold(
       body: Center(
         child: Column(
@@ -42,52 +35,47 @@ class MainScreen extends StatelessWidget {
                       Text(
                         "name:  " +
                             _productcontroller.productModel!.value.name!,
-                        style: TextStyle(fontSize: 25),
+                        style: const TextStyle(fontSize: 25),
                       ),
                       Text(
                         "prise:  " +
                             _productcontroller.productModel!.value.prise!,
-                        style: TextStyle(fontSize: 25),
+                        style: const TextStyle(fontSize: 25),
                       ),
                       Text(
                         "off:  " + _productcontroller.productModel!.value.off!,
-                        style: TextStyle(fontSize: 25),
+                        style: const TextStyle(fontSize: 25),
                       )
                     ],
                   ),
                 )),
-            SizedBox(
+            const SizedBox(
               height: 40,
             ),
             ElevatedButton(
-                style: ButtonStyle(
+                style: const ButtonStyle(
                     foregroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 255, 255, 255)),
+                        Color.fromARGB(255, 255, 255, 255)),
                     backgroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 86, 87, 87))),
+                        Color.fromARGB(255, 86, 87, 87))),
                 onPressed: () {
                   _productcontroller.productModel!.update((vle) {
-                    _productcontroller.productModel!.value.name = "ali";
-                    _productcontroller.productModel!.value.prise = "536800";
-                    _productcontroller.productModel!.value.off = "20";
+                    _productcontroller.productModel!.value.name = "orange";
                   });
                 },
-                child: Text("press to change ")),
-            SizedBox(
-              height: 10,
-            ),
+                child: const Text("press")),
             ElevatedButton(
-                style: ButtonStyle(
+                style: const ButtonStyle(
                     foregroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 255, 255, 255)),
+                        Color.fromARGB(255, 255, 255, 255)),
                     backgroundColor: WidgetStatePropertyAll(
-                        const Color.fromARGB(255, 86, 87, 87))),
+                        Color.fromARGB(255, 86, 87, 87))),
                 onPressed: () {
-                  Get.to(ProductScreen());
+                  Get.to(    MainScreen());
                 },
-                child: Text("next page")),
-            SizedBox(
-              height: 35,
+                child: const Text("backt page")),
+            const SizedBox(
+              height: 40,
             ),
             Container(
               height: 35,
@@ -95,11 +83,11 @@ class MainScreen extends StatelessWidget {
               decoration: BoxDecoration(
                   color: Colors.black26,
                   borderRadius: BorderRadius.circular(12)),
-              child: Row(
+              child: const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    " page <1>",
+                    " page <2>",
                     style: TextStyle(fontSize: 15),
                   ),
                 ],
